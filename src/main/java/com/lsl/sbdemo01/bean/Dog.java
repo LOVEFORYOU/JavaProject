3da192dcf439e7285696a8702b5df5ef23c289fe
linsl <linsl@weipaitang.com>
2020-11-01 13:07:22 +0800

    读取yml配置文件

---
+++ src/main/java/com/lsl/sbdemo01/bean/Dog.java
@@ -0,0 +1,22 @@
+package com.lsl.sbdemo01.bean;
+
+public class Dog {
+    private String names;
+    private Integer age;
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- src/test/java/com/lsl/sbdemo01/Sbdemo01ApplicationTests.java
+++ src/test/java/com/lsl/sbdemo01/Sbdemo01ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.lsl.sbdemo01;
 
+import com.lsl.sbdemo01.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Sbdemo01ApplicationTests {
 
+    //@Autowired：使用的时候要自动加载，不然值会为null
+    @Autowired
+    Person person;
+
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
